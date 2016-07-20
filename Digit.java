3db440bd865acae09f330c030f59038882894f91
Bharathi1310 <bharathikalai1310@gmail.com>
2016-07-20 12:05:54 +0530

    Create Digit.java

---
+++ Digit.java
@@ -0,0 +1,16 @@
+# bharathisi
+import java.util.Scanner;
+public class Digit {
+	public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	System.out.println("enter the number:");
+	int num=in.nextInt();
+	String s=String.valueOf(num);
+			int a=s.length();
+			{
+				System.out.println("no of digits:"+a);
+			}
+
+	}
+
+}
